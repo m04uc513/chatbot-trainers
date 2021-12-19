343fe3d7349c0fe629fc1b49cf3c94f329236fe5
Akito Fujita <akito_fujita@mvg.biglobe.ne.jp>
2021-12-20 00:05:10 +0900

    message editting function

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ app.post("/check", (reqest, response) => {
 // kvs.get
 app.post("/get", (reqest, response) => {
   var key = reqest.body.key;
-  console.log("get: key = %s", key);
+  //console.log("get: key = %s", key);
   db.getRule(key, (rows) => {
-    console.log(rows);
+    //console.log(rows);
     if (rows.length == 0) {
       response.send({
         message: "not found"
@@ -67,8 +67,8 @@ app.post("/get", (reqest, response) => {
 app.post("/put", (reqest, response) => {
   var key = reqest.body.key;
   var val = reqest.body.val;
-  console.log("put: key = %s", key);
-  console.log("put: val = %s", val);
+  //console.log("put: key = %s", key);
+  //console.log("put: val = %s", val);
   db.putRule(key, val, (rows) => {
     response.send({
       message: "success"
@@ -80,55 +80,14 @@ app.post("/put", (reqest, response) => {
 app.post("/set", (reqest, response) => {
   var key = reqest.body.key;
   var val = reqest.body.val;
-  console.log("set: key = %s", key);
-  console.log("set: val = %s", val);
+  //console.log("set: key = %s", key);
+  //console.log("set: val = %s", val);
   db.setRule(key, val, (rows) => {
     response.send({
       message: "success"
     });  
   });
 });
-
-/*
-// endpoint to get all the dreams in the database
-app.get("/getDreams", (request, response) => {
-  console.log(`getDreams:`);
-///   db.getDreams(rows => {
-///     response.send(JSON.stringify(rows));
-///   });
-  response.send({ message: "success" });
-});
-
-// endpoint to add a dream to the database
-app.post("/addDream", (request, response) => {
-  console.log(`addEreams:`);
-///   const cleansedDream = cleanseString(request.body.dream);
-///   db.addDreams(cleansedDream, (error) =>{
-///     if (error) {
-///       response.send({ message: "error!" });
-///     } else {
-  response.send({ message: "success" });
-///     }  
-///   });
-});
-
-// endpoint to clear dreams from the database
-app.get("/clearDreams", (request, response) => {
-  console.log(`clearEreams:`);
-///   db.clearDreams((error) => {
-///     if (error) {
-///       response.send({ message: "error!" });
-///     } else {
-  response.send({ message: "success" });
-///     }  
-///   });
-});
-
-// helper function that prevents html/css/script malice
-const cleanseString = function(string) {
-  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-};
-*/
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
